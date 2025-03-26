63eb3783467182c168f2afef6dcf9c4ce9730883
Kayky Stiliano <kaykystiliano@outlook.com>
2025-03-26 17:46:16 -0300

    Criação da função de zipagem do dataset

--- transformação_dados.py
+++ transformação_dados.py
@@ -1,8 +1,9 @@
+import os
 import pdfplumber
 import pandas as pd
 import zipfile
 
-def extrair_tabela_pdf(pdf_path):
+def extrair_tabela_PDF(pdf_path):
     data = []
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
@@ -16,3 +17,12 @@ def extrair_tabela_pdf(pdf_path):
     df.to_csv(csv_path, index=False, encoding="utf-8")
     return csv_path
 
+def zip_csv(csv_file, zip_name="Teste_seu_nome.zip"):
+    with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(csv_file, os.path.basename(csv_file))
+    print(f"Arquivo CSV compactado em {zip_name}")
+
+if __name__ == "__main__":
+    pdf_path = "downloads/Anexo_I.pdf"  # Ajuste para o caminho correto
+    csv_file = extrair_tabela_PDF(pdf_path)
+    zip_csv(csv_file)
